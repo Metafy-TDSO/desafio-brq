5cc3aaab953b0d87c2f84542f6bb470157c6af37
Guilherme Vieira <guivpw68@gmail.com>
2022-04-30 18:08:40 -0300

    feat: add find by ids of skills in certification method

--- src/main/java/com/fiap/brq/repositories/CertificacaoRepository.java
+++ src/main/java/com/fiap/brq/repositories/CertificacaoRepository.java
@@ -1,9 +1,13 @@
 package com.fiap.brq.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fiap.brq.entities.Certificacao;
 
 public interface CertificacaoRepository extends JpaRepository<Certificacao, Long> {
     Certificacao findByNomeCertificacao(String nome);
+
+    List<Certificacao> findBySkills_IdIn(List<Long> ids);
 }
