9db68e46b64140a1130c086d79844ecfcc76c5b8
Guilherme Vieira <guivpw68@gmail.com>
2022-04-30 18:08:55 -0300

    feat: add find candidates and find by id and skills methods

--- src/main/java/com/fiap/brq/repositories/CandidatoRepository.java
+++ src/main/java/com/fiap/brq/repositories/CandidatoRepository.java
@@ -1,13 +1,23 @@
 package com.fiap.brq.repositories;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.fiap.brq.entities.Candidato;
 
 public interface CandidatoRepository extends JpaRepository<Candidato, Long> {
-    @Query(value = "SELECT * FROM tb_candidato cd WHERE (UPPER(cd.nome) LIKE UPPER(CONCAT('%', ?1, '%')) OR cd.email = ?1 OR cd.cpf = ?1) ORDER BY cd.nome ASC", nativeQuery = true)
-    List<Candidato> buscarTudo(String conteudo);
+    boolean existsByEmailOrCpf(String email, String cpf);
+    @Query("SELECT candidato FROM Candidato candidato "
+			+ "WHERE (:query IS NULL OR ("
+                + "UPPER(candidato.nome) LIKE UPPER(CONCAT('%', :query, '%')) "
+                + "OR candidato.email = :query "
+                + "OR candidato.cpf = :query)"
+            + ")"
+	    )
+    List<Candidato> findByQuery(@Param("query") String query);
+	List<Candidato> findByIdInAndSkills_IdIn(Collection<Long> ids, Collection<Long> ids1);
 }
