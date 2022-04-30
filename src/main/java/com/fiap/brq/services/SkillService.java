e2745d3f0e640dafe5fcd314911f5ce0cfe06536
Guilherme Vieira <guivpw68@gmail.com>
2022-04-30 18:10:46 -0300

    feat: add find or create by name or by many names methods

--- src/main/java/com/fiap/brq/services/SkillService.java
+++ src/main/java/com/fiap/brq/services/SkillService.java
@@ -1,5 +1,8 @@
 package com.fiap.brq.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +14,32 @@ public class SkillService {
     @Autowired
     private SkillRepository repository;
 
+    public List<Skill> findManyByNames(List<String> skills) {
+		List<String> lowercasedSkills = new ArrayList<>();
+
+		for (String skill : skills) {
+			lowercasedSkills.add(skill.toLowerCase());
+		}
+
+		return repository.findByNomeSkillInIgnoreCase(lowercasedSkills);
+    };
+
     public Skill findOrCreateByName(String name) {
-	Skill skill = repository.findByLinguagem(name);
+	Skill skill = repository.findByNomeSkill(name);
 	if (skill == null)
 	    skill = repository.save(new Skill(null, name));
 	return skill;
-    };
+    }
+
+    public List<Skill> findOrCreateManyByName(List<String> input) {
+	List<Skill> dbSkills = new ArrayList<>();
+
+	for (String skill : input) {
+	    dbSkills.add(this.findOrCreateByName(skill));
+	}
+
+	return dbSkills;
+    }
 
     public Skill insert(Skill skill) {
 	return repository.save(skill);
