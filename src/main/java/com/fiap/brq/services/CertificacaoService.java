048e670bced4d9dd13eaa1da0a407dc4e0de32bc
Guilherme Vieira <guivpw68@gmail.com>
2022-04-30 18:10:33 -0300

    feat: update current methods and add find or create many

--- src/main/java/com/fiap/brq/services/CertificacaoService.java
+++ src/main/java/com/fiap/brq/services/CertificacaoService.java
@@ -1,10 +1,12 @@
 package com.fiap.brq.services;
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fiap.brq.dto.CreateCertificacaoDTO;
 import com.fiap.brq.entities.Certificacao;
 import com.fiap.brq.entities.Skill;
 import com.fiap.brq.repositories.CertificacaoRepository;
@@ -14,14 +16,29 @@ public class CertificacaoService {
     @Autowired
     private CertificacaoRepository repository;
 
-    public Certificacao findOrCreateByName(String name, Set<Skill> skills) {
+    @Autowired
+    private SkillService skillService;
+
+    public List<Certificacao> findManyBySkillsIds(List<Long> ids) {
+	    return repository.findBySkills_IdIn(ids);
+    }
+
+    public Certificacao findOrCreateByName(String name, List<Skill> skills) {
 	Certificacao cert = repository.findByNomeCertificacao(name);
 	if (cert == null)
 	    cert = repository.save(new Certificacao(null, name, skills));
 	return cert;
-    };
+    }
 
-    public Certificacao insert(Certificacao certificacao) {
-	return repository.save(certificacao);
+    public List<Certificacao> findOrCreateMany(List<CreateCertificacaoDTO> input) {
+	List<Certificacao> dbCerts = new ArrayList<>();
+
+	for (CreateCertificacaoDTO cert : input) {
+	    List<Skill> dbCertSkills = skillService.findOrCreateManyByName(cert.getSkills());
+
+	    dbCerts.add(this.findOrCreateByName(cert.getNomeCertificacao(), dbCertSkills));
+	}
+
+	return dbCerts;
     }
 }
